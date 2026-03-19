65eb0cbcdc8caf488a50f17b9b356b541eae0480
Senthilkumar Panneerselvam <senthilkumar.panneerselvam@scilifelab.se>
2026-03-19 13:39:20 +0100

    Add NavigationMenu snippet and related models

--- cms/snippets/__init__.py
+++ cms/snippets/__init__.py
@@ -1,1 +1,9 @@
 """Wagtail CMS snippets."""
+
+from .navigation_menu import NavigationMainMenu, NavigationMenu, NavigationSubMenu
+
+__all__ = [
+    "NavigationMainMenu",
+    "NavigationMenu",
+    "NavigationSubMenu",
+]
