ff92d87181e3a6b5615c0a00c2cd64267f629b2a
Ziip-dev <62331138+Ziip-dev@users.noreply.github.com>
2026-03-20 18:21:10 +0100

    refactor: simplify SectionIndexPage by removing section_type field
    SECTION_PAGE_TYPES was unnecessary since the only section type was
    "default" mapping to StandardPage. Inline the allowed child page
    type directly and drop the now-unused CharField.

--- cms/pages/section_index.py
+++ cms/pages/section_index.py
@@ -1,38 +1,25 @@
 """A section index page model."""
 
-from django.db import models
 from wagtail.admin.panels import FieldPanel
 from wagtail.blocks import RichTextBlock
 from wagtail.fields import StreamField
 from wagtail.models import Page
 
 
-# Dict to set allowed child page type for specific section type
-SECTION_PAGE_TYPES = {
-    "default": "cms.StandardPage",
-}
-
-
 class SectionIndexPage(Page):
-    """Generic section page that restricts allowed child page types.
+    """Generic section landing page.
+
+    Acts as a navigational grouping under the homepage.
+    Allowed children:
+        - ``StandardPage``
 
     Attributes:
-        section_type (str): The type of this section, used to determine allowed child pages.
         content (StreamField): Rich text content for the section.
     """
 
-    # List of available section types as (key, display_name) tuples.
-    SECTION_TYPES = [(k, k.capitalize()) for k in SECTION_PAGE_TYPES]
-
     template = "cms/pages/section_index.html"
     parent_page_types = ["cms.HomePage"]
-    subpage_types = list({*SECTION_PAGE_TYPES.values()})
-
-    section_type = models.CharField(
-        max_length=20,
-        choices=SECTION_TYPES,
-        default="default",
-    )
+    subpage_types = ["cms.StandardPage"]
 
     content = StreamField(
         [
@@ -48,6 +35,5 @@ class SectionIndexPage(Page):
     )
 
     content_panels = Page.content_panels + [
-        FieldPanel("section_type"),
         FieldPanel("content"),
     ]
