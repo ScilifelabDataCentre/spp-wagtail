eabd4d53677a9be627253cea606c3147bb55db27
Senthilkumar Panneerselvam <senthilkumar.panneerselvam@scilifelab.se>
2026-04-30 12:27:20 +0200

    Add outbreak pages

--- cms/pages/__init__.py
+++ cms/pages/__init__.py
@@ -1,7 +1,14 @@
 """Wagtail CMS Page models."""
 
 from .home import HomePage
+from .outbreaks import OutbreakPage, OutbreaksIndexPage
 from .section_index import SectionIndexPage
 from .standard_page import StandardPage
 
-__all__ = ["HomePage", "SectionIndexPage", "StandardPage"]
+__all__ = [
+    "HomePage",
+    "OutbreakPage",
+    "OutbreaksIndexPage",
+    "SectionIndexPage",
+    "StandardPage",
+]
