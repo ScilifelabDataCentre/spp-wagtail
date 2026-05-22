169e67635f497e0c7c4945392ad0cc75ff552e77
Senthilkumar Panneerselvam <5311007+senthil10@users.noreply.github.com>
2026-05-22 16:44:36 +0200

    FREYA-2425: Move page types to individual files (#38)

--- cms/pages/__init__.py
+++ cms/pages/__init__.py
@@ -2,19 +2,21 @@
 
 from .catalogue import CataloguePage
 from .highlights_and_editorials import (
-    HighlightsAndEditorialsIndexPage,
     HighlightsAndEditorialsPage,
     HighlightsAndEditorialsTopic,
 )
+from .highlights_and_editorials_index import HighlightsAndEditorialsIndexPage
 from .home import HomePage
 from .news import NewsPage
 from .news_index import NewsIndexPage
-from .outbreaks import OutbreakPage, OutbreaksIndexPage
+from .outbreaks import OutbreakPage
+from .outbreaks_index import OutbreaksIndexPage
 from .plp_index import PlpIndexPage
 from .plp_project import PlpProjectPage
 from .section_index import SectionIndexPage
 from .standard_page import StandardPage
-from .topics import TopicPage, TopicsIndexPage
+from .topics import TopicPage
+from .topics_index import TopicsIndexPage
 
 __all__ = [
     "CataloguePage",
