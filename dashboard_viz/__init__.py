df955042cb1f7bab8b2b59b9c6ac33a9de6b6e75
Abdullah Aziz <mister.abdullah.aziz@gmail.com>
2026-05-27 23:46:33 +0200

    refactor(dashboard): remove unused figure_to_json import from dashboard visualization module

--- dashboard_viz/__init__.py
+++ dashboard_viz/__init__.py
@@ -1,9 +1,7 @@
 """Dashboard visualization services."""
 
 from .registry import generate_figures
-from .utils import figure_to_json
 
 __all__ = [
-    "figure_to_json",
     "generate_figures",
 ]
