cd2146941c04f231d6368f3e4cc77ab111a3e4be
Rickard Hammarén <rickard.hammaren@scilifelab.uu.se>
2026-06-02 11:08:12 +0200

    Rebase

--- cms/pages/__init__.py
+++ cms/pages/__init__.py
@@ -1,24 +1,44 @@
 """Wagtail CMS Page models."""
 
+from .catalogue import CataloguePage
+from .dashboard import DashboardPage
+from .dashboard_index import DashboardIndexPage
 from .highlights_and_editorials import (
     HighlightsAndEditorialsPage,
     HighlightsAndEditorialsTopic,
 )
 from .highlights_and_editorials_index import HighlightsAndEditorialsIndexPage
 from .home import HomePage
+from .news import NewsPage
+from .news_index import NewsIndexPage
 from .outbreaks import OutbreakPage
+from .outbreaks_index import OutbreaksIndexPage
+from .plp_index import PlpIndexPage
+from .plp_project import PlpProjectPage
 from .portal_data import PortalDataPage
 from .section_index import SectionIndexPage
 from .standard_page import StandardPage
 from .topics import TopicPage
+from .topics_index import TopicsIndexPage
 
 __all__ = [
+    "CataloguePage",
+    "DashboardIndexPage",
+    "DashboardPage",
     "HighlightsAndEditorialsIndexPage",
     "HighlightsAndEditorialsPage",
     "HighlightsAndEditorialsTopic",
     "HomePage",
-    "outbreaks",
+    "NewsIndexPage",
+    "NewsPage",
+    "OutbreakPage",
+    "OutbreaksIndexPage",
+    "PlpIndexPage",
+    "PlpProjectPage",
     "PortalDataPage",
     "SectionIndexPage",
     "StandardPage",
-    "TopicPage",]
+    "TopicPage",
+    "TopicsIndexPage",
+]
+
